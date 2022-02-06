3a4576a14f3e5edb1ba8013d11b7109d825c4bb2
Eduarda Machado <duda.r.mach@gmail.com>
2022-02-06 16:38:57 -0300

    database criado

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,34 @@
 #Para rodar de forma automática usamos esse __init__.py
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+#criando database
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'projeto teste'
-    #segurança do app
+    #configurar database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
+    #segurança do app
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/') #sem prefixo
 
+    from .models import User, Note
+
+    create_database(app)
+
     return app
+
+#criando database
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
+from .models import User
+from werkzeug.security import generate_password_hash,check_password_hash
+
 #para definir que teremos vários caminhos auqi
 
 auth = Blueprint('auth', __name__)
@@ -32,5 +35,7 @@ def signup():
             flash('Password must be at least 7 characters.', category ='error')
         else:
             #add user to databases
-            pass
+            new_user = User(email=email, firstName=firstName,password=generate_password_hash(password1, method='sha256'))
+            flash('Account created!', category='success')
+
     return render_template("sign_up.html")
